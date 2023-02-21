7488dab32da2a487aaa0dfbb226872caf81a7259
Bunch-of-cells <alumin112.welcome@gmail.com>
2023-02-21 14:17:45 +0530

    window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use iced::theme::Theme;
+use iced::widget::{container, text};
+use iced::{executor, Length};
+use iced::{Application, Command, Element, Settings};
+
+fn main() -> iced::Result {
+    Rixel::run(Settings::default())
+}
+
+struct Rixel {}
+
+#[derive(Debug, Clone)]
+enum Message {}
+
+impl Application for Rixel {
+    type Message = Message;
+    type Executor = executor::Default;
+    type Theme = Theme;
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Self, Command<Message>) {
+        (Rixel {}, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Rixel")
+    }
+
+    fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            _ => (),
+        }
+
+        Command::none()
+    }
+
+    fn view(&self) -> Element<Message> {
+        container(text("Rixel"))
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_x()
+            .center_y()
+            .into()
+    }
 }
